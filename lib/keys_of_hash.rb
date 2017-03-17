13bc003d7a6eea8578348c7522904f866343f302
Marianna Katsika <marianna.katsika@gmail.com>
2017-03-17 22:45:40 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    arguments.each do |i|
+      self.each do |animal, location|
+      if location == i
+        array << animal
+        end
+      end
+    end
+    array
   end
 end
