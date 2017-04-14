e23fd8abb440b526f46491e7372c4f8aafdbf30d
drzly777 <andrew.zheng725@gmail.com>
2017-04-14 15:46:40 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,8 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    keys_of_array = []
+    self.each{|key,value| keys_of_array << key if arguments.member?(value)}
+    keys_of_array
   end
 end
