4aa73ba4b7360722a7f17fd06262fcd0289869a2
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-05-01 01:33:43 -0500

    Quit monkeying around

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array_to_return = []
+    binding.pry
+    self.each do |key, value|
+      binding.pry
+      if arguments.include?(value)
+        binding.pry
+        array_to_return << key
+      end
+    end
+    array_to_return
   end
 end
