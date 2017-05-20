d4e3b2930fb8c22029456286fccf55bee68125b0
Jason  Rosso <jasonrosso@gmail.com>
2017-05-20 06:02:53 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    return arguments
+  arg_array_to_s = arguments.join("")
+   animals.keys.each do |key|
+     if animals[key] == arg_array_to_s
+       puts key.split("  ")
+       return key.split("  ")
+     end
+   end
   end
 end
