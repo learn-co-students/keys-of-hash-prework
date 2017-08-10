f122895514c1f41cf290e58216f0d754e73fc2b8
Al Artesona <artesonaa2@gmail.com>
2017-08-10 00:48:21 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,14 +1,5 @@
 class Hash
-  def keys_of(arguments, *other_arguments)
-    # code goes here
-    empty_array = []
-    self.each do |key, value|
-      if value == arguments
-        empty_array.push(key)
-      elsif value == other_arguments[0]
-        empty_array.push(key)
-      end
-    end
-    return empty_array
+  def keys_of(*args)
+    map {|key, value| args.include?(value) ? key : nil }.compact
   end
 end
