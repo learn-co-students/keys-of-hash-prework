5f87982c3088653b8ef26683ff7b72962c6f2a10
jonopens <jonopenshaw@gmail.com>
2017-08-19 23:02:41 -0400

    completed first monkey patch lab on keys_of method

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+  	def keys_of(*arguments)
+    	argo_hash = {}
+    	arguments.each do |argo|
+    		self.each do |key, val|
+    			if val == argo
+    				argo_hash[key] = val					
+				end
+			end
+		end
+		key_arr = argo_hash.keys
+		key_arr
+  	end
 end
