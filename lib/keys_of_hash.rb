2578b0befdd3a1d1d06473f32fb7239a8b2480be
github username <github email address>
2017-08-25 14:23:00 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,24 @@
+
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*args)
+	  found_keys = []
+
+
+    args.each { |arg|
+      # fill found_keys with everything we find
+      self.each_pair { |key, value|
+        if (arg == value)
+          found_keys.push(key)
+        end
+      }
+    }
+
+    return found_keys
   end
 end
+
+
+myHash = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
+
+myHash.keys_of("Madagascar", "Australia")
