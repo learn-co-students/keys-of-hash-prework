b48cdd3d77df7f488a393e0c4299dfc638e44928
Ayreal <aeh.herman@gmail.com>
2017-09-04 20:56:47 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+require 'pry'
+# let(:animals) {
+# {"sugar glider"=>"Australia","aye-aye"=> "Madagascar",
+# "red-footed tortoise"=>"Panama","kangaroo"=> "Australia",
+# "tomato frog"=>"Madagascar","koala"=>"Australia"} }
+
+#for each key, is the value included?
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*values)
+    keys = []
+    self.each do |animal_str, country_str|
+      if values.include?(country_str)
+        keys.push(animal_str)
+        end
+      end
+
+    keys
   end
 end
