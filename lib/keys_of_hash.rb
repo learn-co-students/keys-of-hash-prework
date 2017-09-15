05e2471c71ad5f1b6fb8ec85b4f55401b8965e75
Dick Ward <dick@thedickward.com>
2017-09-15 13:59:49 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    results_array = []
+    self.each do |k, v|
+      arguments.each do |argument_value|
+        if argument_value == v
+          results_array << k
+        end
+      end
+    end
+    results_array
   end
 end
