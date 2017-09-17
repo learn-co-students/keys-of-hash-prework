66ea6a8e76d7fbcbecfc2bffd1ba5638d9544a08
Shirley Lin <s.lin12@gmail.com>
2017-09-17 00:16:30 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    answer = []
+    arguments.each do |string|
+      self.each do |animal, place|
+        answer << animal if place == string
+      end
+    end
+    answer
   end
 end
