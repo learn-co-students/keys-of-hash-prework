f4c009e3018ff179c79f9b4a1c93969aa404a7ca
psarma89 <priyam.sarma@gmail.com>
2017-10-09 11:16:31 -0400

    Add keys_of_hash.rb solution file

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    output = []
+    arguments.each do |argument|
+      self.each{|key,value| output << key if value == argument}
+    end
+    output
   end
 end
