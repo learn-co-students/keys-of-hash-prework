2c6589b54e247df99f1077187fffb91665b73a84
Nemanja Drakulovic <drdrakula96@hotmail.com>
2017-11-02 01:56:03 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,18 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    clas = self
+    arguments.each do |value|
+      while key = clas.key(value)
+        array.push(key)
+        clas.delete(key)
+      end
+    end
+    array
   end
 end
+
+
+test = Hash.new
+
+puts test.keys_of("Australia")
