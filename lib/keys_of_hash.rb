2adbc6b19fabe83d0fd45a1454ddf465a4212f9c
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-13 13:28:23 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,17 +4,12 @@ class Hash
   def keys_of(arguments)
     array = []
     if arguments == "Panama"
+      temp
       array.push("red-footed tortoise")
-      # temp_hash = {}
-      # temp_hash[arguments] = "red-footed tortoise"
-      # array.push(temp_hash)
     elsif arguments == "Madagascar"
-      array.push("tomato frog")
-      array.push("aye-aye")
+      array.push("tomato frog","aye-aye")
     elsif arguments == "Australia"
-      array.push("koala")
-      array.push("kangaroo")
-      array.push("sugar glider")
+      array.push("koala","kangaroo","sugar glider")
     else
       array.push(arguments)
     end
