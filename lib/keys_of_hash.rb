b9aa18e6693b2e52269f538e5d1fced094310d15
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-13 14:21:06 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,13 +4,12 @@ class Hash
   def keys_of(arguments)
     array = []
     if arguments == "Panama"
-      temp
       array.push("red-footed tortoise")
     elsif arguments == "Madagascar"
       array.push("tomato frog","aye-aye")
     elsif arguments == "Australia"
       array.push("koala","kangaroo","sugar glider")
-    else
+    elsif
       array.push(arguments)
     end
     return array
