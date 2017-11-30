b86ba4e67d894d41974ebebabc8cfa0f19cf9c64
TorreJohnson <torre.johnson@gmail.com>
2017-11-29 22:21:50 -0500

    defined #keys_of_hash

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    arr = []
+    each do |key, value|
+      arguments.each do |x|
+        if x == value
+          arr << key
+        end
+      end
+    end
+    arr
   end
 end
