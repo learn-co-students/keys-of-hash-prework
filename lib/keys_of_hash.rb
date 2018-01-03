32b73f7f835642c6cefdab6868db2218964d1221
Christopher Diep <diep.christopher@gmail.com>
2018-01-03 21:44:38 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    values_array = [*arguments]
+    values_array.map do |e|
+      self.map do |k,v|
+        if v == e
+          k
+        end
+      end
+    end.flatten.compact
   end
 end
