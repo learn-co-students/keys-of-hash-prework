d115ed168767bc1764a8021677713826ac7688fd
Caroline Lee <carolinel2610@gmail.com>
2018-01-17 17:56:49 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    arguments.each{|arg|
+      self.each{|k,v|
+        if v==arg
+          array << k
+        end
+      }
+    }
+    array
   end
 end
