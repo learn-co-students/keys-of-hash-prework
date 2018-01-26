50d21a5542f276853f2a5053fc94e6af481d63cd
Barney Fabian <bafa@tutanota.de>
2018-01-26 15:48:26 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,9 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    self.each do |x, y|
+      array << x if arguments.include?(y)
+    end
+    array
   end
 end
