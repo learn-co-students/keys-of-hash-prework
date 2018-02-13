b748250ca05618c9e06b505dab1ba6731416ebe7
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-13 19:49:03 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*args)
+    out = []
+    args.each do |value|
+      self.each do |key, word|
+        if word == value
+          out << key
+        end
+      end
+    end
+    out
   end
 end
