5f69077ad6ad3d5184edd5a2494947ae15834cdb
github username <github email address>
2018-03-18 20:02:52 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+def keys_of(*arguments)
+  new_array = []
+  self.each do |key, value|
+    arguments.each do |argument|
+      if argument == value
+        new_array << key
+      end
+    end
   end
+  new_array
+end
 end
