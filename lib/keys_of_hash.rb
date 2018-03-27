525be9a4b4a25c09fe4a94b40906fef6a25cf771
Ryan O'Toole <otoolerj@gmail.com>
2018-03-27 03:38:09 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
+require 'pry'
+
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    # binding.pry
+    # self.keys
+    array = []
+    self.collect do |key, value|
+      if arguments.include?(value)
+        array<<key
+      end
+    end
+array
   end
 end
