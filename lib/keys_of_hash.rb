3a19934437077977bf93a23edbcbb9d4fff5b5db
Ian Canino <ian.canino@gmail.com>
2018-04-23 00:18:58 -0400

    implemented Hash.keys_of class method

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    k_array = []
+    arguments.each do |arg|
+      k_array << self.select { |k, v| v == arg}.keys
+    end
+    return k_array.flatten
   end
 end
