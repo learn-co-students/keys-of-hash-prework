d62fcc3d039edeae9828dc130b54927d823a4dd2
github username <github email address>
2018-05-25 21:51:42 -0700

    built hash class

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*args)
+    return_arr = []
+    args.each do |animal|
+      each do |k, v|
+        if v == animal
+          return_arr.push(k)
+        end
+      end
+    end
+    return_arr
   end
 end
