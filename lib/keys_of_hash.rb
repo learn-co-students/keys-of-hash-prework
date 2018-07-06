84ef675af18156268b9fec4ce5b00dfd6e20d993
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-06 18:00:35 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
-end
+  def keys_of(*arguments)
+    array = []
+    
+    self.each {|animal, country|
+        for i in 0..arguments.length - 1
+          if animal == arguments[i]
+            array << country
+          elsif country == arguments[i]
+            array << animal
+          end
+        end
+            }
+    array
+    end
+ end
