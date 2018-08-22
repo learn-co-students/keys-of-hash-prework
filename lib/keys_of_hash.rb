d119cb5953d082e4d6228626eae36664c0e09a75
Srikant Kumar Kalaputapu <sriki@Srikants-MBP.home>
2018-08-22 16:53:44 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+#Monkey Patching
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+    def keys_of(*arguments)
+        # code goes here
+        keys = []
+        self.each do |key,value|
+            if arguments.include?(value)
+                keys.push(key)
+            end
+        end
+        keys
+    end
 end
