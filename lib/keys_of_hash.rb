ec51b48cfb7e73aab2cb14b747a1011458099df5
Eric Klimowich <you@example.com>
2018-09-15 13:43:44 -0400

    KoH

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
-  end
+    animal_array = []
+      arguments.each do |argument|
+        self.each do |animal, location|
+          if location == argument
+            animal_array << animal
+          elsif animal ==  argument.to_i
+            animal_array << animal
+          end
+        end
+      end
+      animal_array
+    end
 end
