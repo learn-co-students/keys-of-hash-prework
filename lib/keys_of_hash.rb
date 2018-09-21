34a8e5e10dffaea71730706203130837523dce07
Joseph Arias <arias.joseph.a@gmail.com>
2018-09-21 13:42:59 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
-end
+  def keys_of(*arguments)
+    # code goes here	    
+    array = []
+    self.each do |key, value|
+    
+      if arguments.include?(value)
+        array.push(key)
+      end
+    end
+    return array
+  end	
+end 	
