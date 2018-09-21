9365609ff14659ba0a2213d6f05a2642c5e9310c
Rob Parker <into.the.parker@gmail.com>
2018-09-21 15:26:10 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
+require "pry"
+
 class Hash
   def keys_of(arguments)
-    # code goes here
-  end
+    
+  end  
 end
+
+# {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
+    
