db902b624a0f267b139c5dab7725a6ef1b69f47e
Austin Luft <ausluft@gmail.com>
2018-09-26 16:28:26 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,32 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array =[]
+
+    # hash = {
+    #   "red-footed tortoise" => "Panama",
+    #   "aye-aye" => "Madagascar",
+    #   "tomato frog" => "Madagascar",
+    #   "sugar glider" => "Australia",
+    #   "kangaroo" => "Australia",
+    #   "koala" => "Australia",
+    #   a: 1, 
+    #   b: 2, 
+    #   c: 3, 
+    #   d: 1
+    # }
+
+#    binding.pry
+    
+    self.each do |k, v|
+      arguments.each do |x|
+      if x == v
+        array.push(k)
+      end
+    end
+  end
+
+    return array
   end
 end
