7d6667aea7d13cb04a343d4edc86ebcf91514f5c
github username <github email address>
2018-09-27 18:40:49 -0400

    Keys Of Hash

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    animal_array = []
+    self.each do |animal, place|
+      if arguments.include?(place)
+        animal_array << animal
+      end
+    end
+  animal_array
   end
 end
