56145a89f9654deb5e5ed6cedb796a5182c5622f
vera protopopova <vera.protopopova@gmail.com>
2018-10-03 20:25:04 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    animal_array = []
+    self.each do |animal, place|
+      if arguments.include?(place)
+        animal_array << animal
+      end
+    end
+  animal_array
   end
 end
