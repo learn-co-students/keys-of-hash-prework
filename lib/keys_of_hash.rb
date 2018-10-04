8295c49a42653a883a30f8ce1dd88a31be8c308d
ruthmesfun <ruth.mesfun@gmail.com>
2018-10-04 15:45:36 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*location)
+    keys = []
+   if location.length == 1
+    locations = select{|animal, loc| loc == location[0]}
+    return locations.keys
+   end
+
+   location.each do |locale|
+    locations = select{|animal, loc| loc == locale}
+    keys << locations.keys
+   end
+
+   return keys.flatten
   end
 end
