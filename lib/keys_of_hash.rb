15a8619ad3acf0bc5607d648365584c5fbafd1d2
leannemcabey <leanne.m.cabey@gmail.com>
2018-10-15 19:41:50 -0400

    completed method to pass tests

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    matched_keys = []
+    self.each do |key, value|
+      if arguments.include?(value)
+        matched_keys << key
+      end
+    end
+    matched_keys
   end
 end
