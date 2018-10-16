757aa4cade3c46f55fcb03e9f5cd9902d6905e66
Scott M <gmat191@gmail.com>
2018-10-16 02:54:21 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    key_list=[]
+
+arguments.each do |arg|
+    map do |k,v|
+     if arg == v
+   key_list<< k
+      end
+     end
+    end
+key_list
   end
 end
