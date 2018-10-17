813160d19b224f8e534ee3e5a6fa0f999878f831
Manon Pilaud <mandyypilaud@gmail.com>
2018-10-17 15:05:40 -0700

    C1

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    self.each do |animal,place|
+      if arguments.include?(place)
+        array << animal
+      end
+    end
+    array
   end
 end
