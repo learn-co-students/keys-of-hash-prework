a4f8cf2bd11f6c34549d04d2394d4116b483a22b
Dania Herrera <danherrera0@gmail.com>
2018-12-05 21:53:32 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,19 @@
+require "pry"
 class Hash
   def keys_of(*args)
     map {|key, value| args.include?(value) ? key : nil }.compact
+    #.compact gets rid of nil values
   end
 end
+# final_arr=[]
+# def keys_of(*args)
+#   binding.pry
+# args.each do |key, value|
+# value == args[value]
+#   final_arr << key
+#
+#
+# end
+# end
+# final_arr
+# end
