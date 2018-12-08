f10497fd5e7b4a3e0083acb8be20f253507d61f1
FutureScript <fernandobaxter1@gmail.com>
2018-12-08 15:55:52 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    animals = []
+		self.each { |animal, place|
+			if arguments.include?(place)
+				animals << animal
+			end
+		}
+		return animals
   end
 end
