a9a074500162d2960b0ac64118af349cc726cede
Ross Jones <rossmosis.jones@gmail.com>
2019-01-02 02:06:19 -0500

    created new instance method of the hash class

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    array = []
+
+    self.each do |k, v|
+      if arguments.include?(v)
+        array << k
+      end
+    end
+    array
   end
 end
