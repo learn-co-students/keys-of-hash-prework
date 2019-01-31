ad424aa7d0ecd2cf7d79b9a85b06dcb7e60909ea
stephenyoon84 <stephenyoon84@gmail.com>
2019-01-31 16:16:59 -0500

    Keys-of-hash - Done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    # map {|key, value| arguments.include?(value) ? key : nil }.compact
+    arr = []
+    collect do |key, value|
+      if arguments.include?(value)
+        arr << key
+      end
+    end
+    return arr
   end
 end
