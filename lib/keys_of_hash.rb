db27f94154c6e6ae6293fef20bb56030a4b06879
Burak Kocak <burakkocak884@ghotmail.com>
2019-02-06 21:51:46 -0500

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+  def keys_of(*arguments)
+my_array = [] 
+#binding.pry
+self.each do |key, value|
+#binding.pry
+arguments.each do |info|
+
+	if info == value
+#		binding.pry
+		my_array.push(key)
+	
+end
+end
+end
+my_array
+end
 end
