16f16a29ce4fde7f6c69496ab85de46f96a1596e
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-22 23:45:36 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    # code goes here	    # code goes here
+		animals = []
+		self.each { |animal, place|
+			if arguments.include?(place)
+				animals << animal
+			end
+		}
+		return animals
   end
 end
