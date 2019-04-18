179af159449965a6317ab460c0bd7c1038222a83
Amy Cen <amy.cen@blackrock.com>
2019-04-18 03:11:26 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    keys = []
+    arguments.each do |val|
+      self.map { |k, v| v== val ? keys << k : nil }.compact
+    end
+    keys
   end
 end
