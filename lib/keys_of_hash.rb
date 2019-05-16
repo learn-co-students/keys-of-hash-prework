307d29832726b791938b9d54c84df021271f6124
Roysan Easo <easo.roysan@gmail.com>
2019-05-16 01:56:29 -0500

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  
+  def keys_of(*arguments)
+    answer = self.collect do |key,value|
+      arguments.collect do |tester|
+        if value == tester
+          key
+        end
+      end
+    end
+    answer.flatten!(2)
+    answer.delete(nil)
+    answer
   end
 end
