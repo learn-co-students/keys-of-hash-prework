23b2e571d074bc8200e6a88471418f5656044f23
brinda behal <brindab50@gmail.com>
2019-07-23 17:11:45 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,17 +1,6 @@
 class Hash
   def keys_of(*arguments)
-    if arguments.include?(value)
-      key
-  else
-    nil
+    map {|key, value| arguments.include?(value) ? key : nil }.compact
+  
   end
-  
 end
-
-
-#def keys_of(*args) 
-  #map {|key, value| args.include?(value) ? key : nil }.compact
-  
-#end
- #test-expression ? if-true-expression : if-false-expression
- # let(:animals) { {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
