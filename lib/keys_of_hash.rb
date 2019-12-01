c30401a1e20f6705aead48169563a7e8c16e5cb1
dagonyclark <sean@dagony-clark.com>
2019-12-01 14:02:30 -0500

    Revert "Done."
    This reverts commit 6e4f8ea9a715a58df04181f439c3d5a5544d2c4f.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,22 +1,5 @@
-require 'pry'
-
 class Hash
-  def keys_of(*arguments)
+  def keys_of(arguments)
     # code goes here
-    array = []
-    self.each do |thing1, thing2|
-      # if arguments.join == 1
-      # end
-      counter = 0
-      # binding.pry
-      while counter < arguments.length
-        if thing2 == arguments[counter]
-          array << thing1
-        end
-        counter += 1
-      end
-    end
-    # binding.pry
-    return array
   end
 end
